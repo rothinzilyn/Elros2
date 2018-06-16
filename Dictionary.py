1efd9c4804209b69e5eb0173a231be7684145918
lynorca87 <lynorca87@gmail.com>
2018-06-16 14:34:09 +0900

    make Dictionary class

--- Dictionary.py
+++ Dictionary.py
@@ -1,35 +1,47 @@
 # coding=utf-8
 import random
-en_list = []
-kr_list = []
-true_word = []
-false_word = []
+import dic.txt
 
 
 class Dictionary:
-    def read_from_dictionary(self, first, second):
-        f = open("dic.txt", "r")
-        #dic.txt open
+    en_list = []
+    kr_list = []
+
+    def set_dictionary(self):
+        print('Input (s) when you whant add new word in your dic. Else, input (n).')
+        # 위의 질의에 대한 응답 저장 변수
+        action_whether = input()
+        if action_whether == 's':
+            input_dic_en = input('영어 스펠링을 써주세요. 끝내고 싶으시면 q를 누르세요: ')
+            input_dic_kr = input('한국어 뜻을 써주세요:')
+
+    # kr, en_list 를 바깥으로 빼주는 함수
+    def get_dictionary(self, type):
+        if type == 1:
+            return self.kr_list
+        else:
+            return self.en_list
+
+    def read_from_dictionary(self):
+        # dic.txt open
+        dictionary = open("dic.txt", "r")
         while 1:
-            line = f.readline()
+            line = dic.readline()
             if not line:
                 break
+                # '=' 단어에서 빼기
             line_list = line.split('=')
-            #'=' 단어에서 빼기
-            en_list.append(line_list[0])
-            kr_list.append(line_list[1].strip("\n"))
-        f.close()
-
-    def out_question(self, first, second):
-        c = random.randint(0, 10)
-        print(c)
+            self.en_list.append(line_list[0])
+            # enter 없애줌
+            self.kr_list.append(line_list[1].strip("\n"))
+        dic.close()
 
 
 my_dictionary = Dictionary()
-my_dictionary.out_question(en_list, kr_list)
+my_dictionary.out_question()
 
 
-print("en_list: ", en_list)
-print("kr_list: ", kr_list)
+print("kr_list: ", my_dictionary.get_dictionary(1))
+print("en_list: ", my_dictionary.get_dictionary(2))
 
 
